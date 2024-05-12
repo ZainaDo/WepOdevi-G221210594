636e4108f24d846a1a9ed2feab55864762bae5e6
ZainaDo <zenah.aldous22@gmail.com>
2024-05-12 23:42:34 +0300

    login sayfasi

---
+++ js/login.js
@@ -0,0 +1,21 @@
+function loginDenetle(){
+    var kulAd="G221210594";
+    var sifre="12345";
+    var KullaniciAdi=document.getElementById("kullaniciAdi").value;
+    var Parola=document.getElementById("parola").value;
+    var Hata=document.getElementById("hata");
+
+    if(KullaniciAdi=="" || Parola==""){
+        Hata.style.visibility="visible";
+        Hata.innerHTML="Kullanıcı adı ya da Parola boş bırakmayin.";
+        return false;
+    }
+    else if((KullaniciAdi!="" && KullaniciAdi!=kulAd) || (Parola!="" && Parola!=sifre)){
+        Hata.style.visibility="visible";
+        Hata.innerHTML="Kullanıcı adı ya da Parola yanlış.";
+        return false;
+    }
+    else{
+        return true;
+    }
+}
