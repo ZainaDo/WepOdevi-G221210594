db1ab16b496f4a13efac0f6436c9fde424ae28bf
ZainaDo <zenah.aldous22@gmail.com>
2024-05-18 23:18:53 +0300

    Alanlarim sayfasi

--- php/login.php
+++ php/login.php
@@ -5,23 +5,25 @@ error_reporting(0);
 if(isset($_POST['login'])){
     $kulAd="G221210594";
     $sifre="12345";
-    $kullaniciAdi=$_POST["kullaniciAdi"];
-    $parola=$_POST["parola"];
-    
-    if($kullaniciAdi!="" && $parola!=""){
+    $kullaniciAdi=htmlspecialchars($_POST["kullaniciAdi"]);
+    $parola=htmlspecialchars($_POST["parola"]);
+    if(!empty($kullaniciAdi) && !empty($parola)){
         if($kullaniciAdi==$kulAd && $parola==$sifre){
             echo "Hoşgeldiniz <br><hr>";
             echo "$kullaniciAdi";
         }
         else{
             header("Location:../html/login.html");
+            exit();
         }
     }
     else{
         header("Location:../html/login.html");
+        exit();
     }
 }
 else {
     header("Location:../html/login.html");
+    exit();
 }
 ?>
